4cc0c02f9d77858c11856ba32becab1031329032
joe-no-body <20505941+joe-no-body@users.noreply.github.com>
2024-01-24 06:14:56 -0800

    rename extension and update README and CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,9 @@
 # Change Log
 
-All notable changes to the "save-and-close-tab" extension will be documented in this file.
+All notable changes to the "save-and-close-editor" extension will be documented in this file.
 
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
+## [1.0.0] - 2024-01-24
 
-## [Unreleased]
+### Added
 
-- Initial release
+- New command: "File: Save and Close Editor"
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('save-and-close-tab.saveAndCloseActiveEditor', async () => {
+	let disposable = vscode.commands.registerCommand('save-and-close-editor.saveAndCloseActiveEditor', async () => {
     await vscode.commands.executeCommand("workbench.action.files.save");
     vscode.commands.executeCommand("workbench.action.closeActiveEditor");
 	});
