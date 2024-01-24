ecfca846b4762b2e0f710b0e6c407aa9761d3a4a
joe-no-body <20505941+joe-no-body@users.noreply.github.com>
2024-01-24 06:07:28 -0800

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,13 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('save-and-close-tab.saveAndCloseActiveEditor', async () => {
+    await vscode.commands.executeCommand("workbench.action.files.save");
+    vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
