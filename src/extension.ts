3ec7f8973668426c20d71f4ceeb99f122205135a
joe-no-body <20505941+joe-no-body@users.noreply.github.com>
2024-01-24 07:26:06 -0800

    remove needless debug code

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,7 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	let saveAndCloseActiveEditorDisposable = vscode.commands.registerCommand('save-and-close-editor.saveAndCloseActiveEditor', async () => {
     // save the document if it has changes
-    let isDirty = vscode.window.activeTextEditor?.document.isDirty;
-    console.log(`document isDirty: ${isDirty}`);
-    if (isDirty) {
+    if (vscode.window.activeTextEditor?.document.isDirty) {
       await vscode.commands.executeCommand("workbench.action.files.save");
     }
     vscode.commands.executeCommand("workbench.action.closeActiveEditor");
